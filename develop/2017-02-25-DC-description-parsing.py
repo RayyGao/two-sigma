e5064d179a5b4528454a364571233fd30918360e
dwcoates <dodge.w.coates@gmail.com>
2017-02-27 01:06:41 -0500

    Small change

--- develop/2017-02-25-DC-description-parsing.py
+++ develop/2017-02-25-DC-description-parsing.py
@@ -11,8 +11,8 @@ d = data.description
 d_words = d.apply(word_tokenize)
 d_null = d.apply(lambda x: len(x) > 0)
 
-percent_null = sum(~d_null)/float(sum(d_null))
+percent_null = sum(~d_null) / float(sum(d_null))
 
 dist = data.features.apply(
-        lambda x: pd.Series(map(lambda z: 1 if (z in x) else 0, distinct_features) +
-                            [list(np.setdiff1d(x, distinct_features))]))
+    lambda x: pd.Series(map(lambda z: 1 if (z in x) else 0, distinct_features) +
+                        [list(np.setdiff1d(x, distinct_features))]))
