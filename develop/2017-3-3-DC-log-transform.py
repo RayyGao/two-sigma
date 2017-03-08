ba2450a78e0b605ffd99476e8e7a1b0b7aa5ff2f
Dodge W. Coates <dcoates91@gmail.com>
2017-03-08 13:24:40 -0500

    Add log transform image

--- develop/2017-3-3-DC-log-transform.py
+++ develop/2017-3-3-DC-log-transform.py
@@ -3,6 +3,7 @@ Log transform doesn't seeem appropriate, I don't think.
 """
 
 import pandas as pd
+import numpy as np
 from matplotlib import pylab as plt
 
 data = pd.read_json("../data/train.json")
@@ -11,18 +12,19 @@ man_counts = pd.DataFrame(data.manager_id.value_counts())
 man_counts["manager count"] = man_counts["manager_id"]
 man_counts["manager_id"] = man_counts.index
 
-logs_test = np.log(man_counts["manager count"])
+logs_test = np.log(man_counts["manager count"]).tolist()
 
-print(logs_test.head())
+print(logs_test[:5])
 
 # plot log
-plt.plot(np.log(man_counts["manager count"]), label="natural log")
+plt.plot(np.log(man_counts["manager count"]).tolist()[1:], label="natural log")
 
 # plot log base 10
-plt.plot(np.log10(man_counts["manager count"]), label="log base 10")
+plt.plot(np.log10(man_counts["manager count"]
+                  ).tolist()[1:], label="log base 10")
 
 # plot log base 2
-plt.plot(np.log2(man_counts["manager count"]), label="log base 2")
+plt.plot(np.log2(man_counts["manager count"]).tolist()[1:], label="log base 2")
 
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
            ncol=2, mode="expand", borderaxespad=0.)
